e15bd8fc440266fda20aec2b0677538f13f92424
Fabian Zach <fabian.zach@methodpark.de>
2021-11-23 10:18:47 +0100

    Make page fully dynamic

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 # startpage
 
-Simple browser start page built using [Vue.js](https://vuejs.org) and [tailwindcss](https://tailwindcss.com/).
-Data is loaded from [startpage.js](startpage.js).
+Simple browser start page built using [Vue.js](https://vuejs.org) and [tailwindcss](https://tailwindcss.com).
+Static data can be loaded from [startpage-data.js](startpage-data.js).
+If static data is empty all data is loaded from and stored in [browser local storage](https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage).
+
+> Access this page (data is stored only in your browser) at
+>
+> https://fabianmp.github.io/startpage
+
+## Used libraries
+
+- [Vue.js](https://vuejs.org)
+- [tailwindcss](https://tailwindcss.com)
+- [tippy.js](https://atomiks.github.io/tippyjs)
+- [popper.js](https://popper.js.org)
+- [Font Awesome](https://fontawesome.com)
--- startpage.js
+++ startpage.js
@@ -1,77 +1,210 @@
-var data = {
-  groups: [
-    {
-      name: "First Group",
-      color: ["bg-gray-700", "text-white"],
-      linkGroups: [
-        {
-          name: "Sub Group 1.1",
-          title: ["bg-blue-500", "text-white"],
-          border: ["border-blue-500"],
-          links: [
-            {
-              name: "Vue.js",
-              url: "https://vuejs.org/"
-            },
-            {
-              name: "tailwindcss",
-              url: "https://tailwindcss.com/"
-            }
-          ]
-        },
-        {
-          name: "Sub Group 1.2",
-          title: ["bg-red-500", "text-white"],
-          border: ["border-red-500"],
-          links: [
-            {
-              name: "Apple",
-              url: "https://www.apple.com/"
-            },
-            {
-              name: "Android",
-              url: "https://www.android.com/"
-            }
-          ]
-        },
-        {
-          name: "Sub Group 1.3",
-          title: ["bg-yellow-700", "text-white"],
-          border: ["border-yellow-700"],
-          links: [
-            {
-              name: "Google",
-              url: "https://www.google.com/"
-            },
-            {
-              name: "Microsoft",
-              url: "https://www.microsoft.com"
-            }
-          ]
-        }
-      ]
-    },
-    {
-      name: "Second Group",
-      color: ["bg-blue-400", "text-white"],
-      linkGroups: [
-        {
-          name: "Sub Group 2.1",
-          title: ["bg-blue-700", "text-white"],
-          border: ["border-blue-700"],
-          links: [
-            {
-              name: "Netflix",
-              url: "https://www.netflix.com/"
-            }
-          ]
-        }
-      ]
-    }
-  ]
-};
+const baseColors = ['red', 'yellow', 'green', 'blue', 'purple', 'gray']
+const colors = baseColors.flatMap(c => [300, 600, 800].map(w => `${c}-${w}`))
 
-var app = new Vue({
-  el: '#app',
-  data: data
-});
+const app = Vue.createApp({
+  data() {
+    return {
+      localStorage: true,
+      blocks: [],
+      edit: false,
+      editData: null,
+      showPopup: false
+    }
+  },
+  mounted() {
+    if (data !== undefined && data.length !== 0) {
+      this.blocks.splice(0, this.blocks.length, ...data)
+      this.localStorage = false
+    } else {
+      const data = JSON.parse(window.localStorage.getItem('startpage-data') ?? '[]')
+      this.blocks.splice(0, this.blocks.length, ...data)
+      this.localStorage = true
+    }
+    tippy.createSingleton(tippy('[data-tippy-content]'), {
+      appendTo: () => document.body,
+      delay: [500, 0]
+    })
+  },
+  methods: {
+    showEditPopup(target) {
+      this.editTippy?.destroy()
+      this.editTippy = tippy(target, {
+        appendTo: () => document.getElementById('app'),
+        content: '<div id="edit-popup"></div>',
+        allowHTML: true,
+        hideOnClick: false,
+        interactive: true,
+        ignoreAttributes: true,
+        showOnCreate: true,
+        sticky: 'popper',
+        trigger: 'manual',
+        onHidden: () => {
+          this.showPopup = false
+        },
+        onMount: () => {
+          this.showPopup = true
+        }
+      })
+    },
+    hideEditPopup() {
+      this.editTippy?.destroy()
+      this.editData = null
+      this.showPopup = false
+    },
+    editLink(block, group, link, event) {
+      this.editData = { block, group, link, type: 'link' }
+      this.showEditPopup(event.currentTarget)
+    },
+    deleteLink(block, group, link) {
+      this.blocks[block].groups[group].links.splice(link, 1)
+    },
+    editGroup(block, group, event) {
+      this.editData = { block, group, type: 'title' }
+      this.showEditPopup(event.currentTarget)
+    },
+    deleteGroup(block, group) {
+      this.blocks[block].groups.splice(group, 1)
+    },
+    editBlock(block, event) {
+      this.editData = { block, group: undefined, type: 'title' }
+      this.showEditPopup(event.currentTarget)
+    },
+    deleteBlock(block) {
+      this.blocks.splice(block, 1)
+    },
+    reset() {
+      this.blocks = JSON.parse(window.localStorage.getItem('startpage-data') ?? '[]')
+    },
+    importFile() {
+      document.querySelector("#file-input").click()
+    },
+    loadFileContent(event) {
+      if (event.target.files.length !== 1) {
+        return
+      }
+      const file = event.target.files[0]
+      var reader = new FileReader()
+      reader.addEventListener('load', function (e) {
+        const data = JSON.parse(e.target.result)
+        vm.blocks.splice(0, vm.blocks.length, ...data)
+      })
+      reader.readAsText(file)
+      event.target.value = null
+    },
+    download() {
+      const jsonData = JSON.stringify(JSON.parse(window.localStorage.getItem('startpage-data')), null, 2)
+      var element = document.createElement('a')
+      element.style.display = 'none'
+      element.setAttribute('href', 'data:application/jsoncharset=utf-8,' + encodeURIComponent(jsonData))
+      element.setAttribute('download', 'startpage.json')
+      document.body.appendChild(element)
+      element.click()
+      document.body.removeChild(element)
+    },
+    accept() {
+      window.localStorage.setItem('startpage-data', JSON.stringify(this.blocks))
+      this.hideEditPopup()
+      this.edit = false
+    }
+  }
+})
+
+app.component("tooltip-dummy", {
+  template: '<span></span>',
+  mounted() {
+    this.allTooltips = tippy('[data-tippy-content]')
+    this.singletonTooltip = tippy.createSingleton(this.allTooltips, {
+      appendTo: () => document.body,
+      delay: [500, 0]
+    })
+  },
+  unmounted() {
+    this.allTooltips.forEach(t => t.destroy())
+    this.singletonTooltip.destroy()
+  }
+})
+
+app.component("edit-title", {
+  template: "#edit-title-template",
+  data() {
+    return {
+      name: '',
+      color: '',
+      colors: colors
+    }
+  },
+  props: {
+    block: Number,
+    group: Number
+  },
+  methods: {
+    close() {
+      this.$root.hideEditPopup()
+    },
+    save() {
+      if (this.block < 0) {
+        this.$root.blocks.push({ name: this.name, color: this.color, groups: [], visible: true })
+      } else if (this.group < 0) {
+        this.$root.blocks[this.block].groups.push({ name: this.name, color: this.color, links: [] })
+      } else if (this.group === undefined) {
+        this.$root.blocks[this.block].name = this.name
+        this.$root.blocks[this.block].color = this.color
+      } else {
+        this.$root.blocks[this.block].groups[this.group].name = this.name
+        this.$root.blocks[this.block].groups[this.group].color = this.color
+      }
+      this.close()
+    }
+  },
+  created() {
+    if (this.block < 0 || this.group < 0) {
+      this.color = this.colors[Math.floor(Math.random() * this.colors.length)]
+      return
+    }
+
+    if (this.group === undefined) {
+      this.name = this.$root.blocks[this.block].name
+      this.color = this.$root.blocks[this.block].color
+    } else {
+      this.name = this.$root.blocks[this.block].groups[this.group].name
+      this.color = this.$root.blocks[this.block].groups[this.group].color
+    }
+  }
+})
+
+app.component("edit-link", {
+  template: "#edit-link-template",
+  data() {
+    return {
+      name: '',
+      url: ''
+    }
+  },
+  props: {
+    block: Number,
+    group: Number,
+    link: Number
+  },
+  methods: {
+    close() {
+      this.$root.hideEditPopup()
+    },
+    save() {
+      if (this.link < 0) {
+        this.$root.blocks[this.block].groups[this.group].links.push({ name: this.name, url: this.url })
+      } else {
+        this.$root.blocks[this.block].groups[this.group].links[this.link].name = this.name
+        this.$root.blocks[this.block].groups[this.group].links[this.link].url = this.url
+      }
+      this.close()
+    }
+  },
+  created() {
+    if (this.link < 0) return
+
+    this.name = this.$root.blocks[this.block].groups[this.group].links[this.link].name
+    this.url = this.$root.blocks[this.block].groups[this.group].links[this.link].url
+  }
+})
+
+const vm = app.mount('#app')
